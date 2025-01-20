dcaffcb27d0dfb6479f77ea67833b5f0084773be
IT21314742 <it21314742@my.sliit.lk>
2025-01-20 23:19:23 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout)
