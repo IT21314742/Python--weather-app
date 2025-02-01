d4159262038e8c393c3324478c29d9617b6a37f5
IT21314742 <it21314742@my.sliit.lk>
2025-02-01 22:43:48 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class WeatherApp(QWidget):
         vbox = QVBoxLayout()
 
         vbox.addWidget(self.city_label)
-        vbox.addWidget(self.city_)
+        vbox.addWidget(self.city_input)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
