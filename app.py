4276e6bee6a65080673e512fae4e3aa536b26ce1
IT21314742 <it21314742@my.sliit.lk>
2025-02-10 22:25:18 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class WeatherApp(QWidget):
         
         self.setStyleSheet("""
             QLabel, QPushButton{
-            font-family: calibri}
+            font-family: calibri;}
         """)
 
 if __name__ == "__main__":
