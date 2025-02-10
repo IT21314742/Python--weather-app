9945bd56b43fe6eee9a519c90b9e63a580172c80
IT21314742 <it21314742@my.sliit.lk>
2025-02-10 22:23:00 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class WeatherApp(QWidget):
         self.description_label.setAlignment(Qt.AlignCenter)
         
         self.setStyleSheet("""
-        
+            QLabel
         """)
 
 if __name__ == "__main__":
