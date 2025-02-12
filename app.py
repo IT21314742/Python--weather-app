eecaff734950c5ae954bdb6298566361f6c5e833
IT21314742 <it21314742@my.sliit.lk>
2025-02-12 21:25:51 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ class WeatherApp(QWidget):
                 font-size: 75px;
             }
             QLabel#emoji_label{
-                font-size: 100px}
+                font-size: 100px;}
 
         """)
 
