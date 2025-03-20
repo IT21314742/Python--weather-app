cbbf4ba188d2d28c7bb99b36146d81d3be793dd0
IT21314742 <it21314742@my.sliit.lk>
2025-03-20 19:37:30 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ class WeatherApp(QWidget):
 
         """)
 
-        self.get_weather_button.clicked
+        self.get_weather_button.clicked.connect()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
