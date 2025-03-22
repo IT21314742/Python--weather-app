68dc43089da0e8dd127c792a0035adf790a0172c
IT21314742 <it21314742@my.sliit.lk>
2025-03-22 07:44:29 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ class WeatherApp(QWidget):
         city = self.city_input.text()
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city }&appid={api_key}"
         
-        
+        response = requests.get(url)
 
     def display_error(self, message):
         pass
