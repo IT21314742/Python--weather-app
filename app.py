cac4d86281b957e275763425ae38c3e07e6bfe96
IT21314742 <it21314742@my.sliit.lk>
2025-03-23 10:10:42 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ class WeatherApp(QWidget):
                 print("Bad request\nPlease check your input")
             case 500:
                 print("Bad request\nPlease check your input")
-            case 400:
+            case 502:
                 print("Bad request\nPlease check your input")
             case 400:
                 print("Bad request\nPlease check your input")
