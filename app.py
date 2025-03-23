735b39cdd1b6bbe2bbd89a473b805170bf862d38
IT21314742 <it21314742@my.sliit.lk>
2025-03-23 09:51:13 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ class WeatherApp(QWidget):
         
     try:
         response = requests.get(url)
-        response
+        response.raise_for_status
         data = response.json()
         
         if data["cod"] == 200:
