726d973308e21c710bc8612dd9fc789d201e5c3a
IT21314742 <it21314742@my.sliit.lk>
2025-03-23 09:48:37 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ class WeatherApp(QWidget):
         if data["cod"] == 200:
             self.display_weather(data)
 
-except HTTP
+    except HTTPError
     def display_error(self, message):
         pass
 
