1256d0cb3462422f0ccec20867580e98cb369c8d
IT21314742 <it21314742@my.sliit.lk>
2025-03-23 10:02:22 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ class WeatherApp(QWidget):
     except requests.exceptions.HTTPError:
         pass
     except requests.exceptions.RequestException:
+        
     def display_error(self, message):
         pass
 
