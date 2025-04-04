ed1a1de9d7868c129c92be983ba15daf39c477f1
IT21314742 <it21314742@my.sliit.lk>
2025-04-04 19:05:52 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ class WeatherApp(QWidget):
     except requests.exceptions.Timeout:
         pass
     except requests.exceptions.ConnectionError:
-        
+        pass
                 
     except requests.exceptions.RequestException:
         pass
