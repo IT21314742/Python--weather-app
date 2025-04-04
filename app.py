f77904c6770a12b2a081868acabb191c77d0a611
IT21314742 <it21314742@my.sliit.lk>
2025-04-04 19:07:46 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ class WeatherApp(QWidget):
                 print(f"HTTP error occured\n{http_error}")
                 
     except requests.exceptions.ConnectionError:
-        pass
+        print
     except requests.exceptions.Timeout:
         pass
     except requests.exceptions.TooManyRedirects:
