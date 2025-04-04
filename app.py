73b8dec8a87b9827fff033a0b8adab94ca265836
IT21314742 <it21314742@my.sliit.lk>
2025-04-04 19:09:00 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ class WeatherApp(QWidget):
     except requests.exceptions.ConnectionError:
         print("Connection Error\nCheck your internet connection")
     except requests.exceptions.Timeout:
-        pass
+        print("Timeout Error")
     except requests.exceptions.TooManyRedirects:
         pass
     except requests.exceptions.RequestException:
