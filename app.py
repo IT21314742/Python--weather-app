5362d537bfa7a3d2900556797a3a8588f3fe4f12
IT21314742 <it21314742@my.sliit.lk>
2025-04-14 14:00:55 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -123,6 +123,8 @@ class WeatherApp(QWidget):
         temperature_k = data["main"]["temp"]
         temperature_c = temperature_k - 273.15
         temperature_f = (temperature_k * 9/5) - 459.67
+        
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
